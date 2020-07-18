5449f066d1163baed4c31d559389158068a56223
Narek <narek.kirakossian@gmail.com>
2020-07-17 21:43:27 -0600

    Add modal window to cart

--- src/Context.js
+++ src/Context.js
@@ -10,6 +10,8 @@ class ProductProvider extends Component {
 		products: [],
 		detailProduct: detailProduct,
 		cart: [],
+		modalOpen: false,
+		modalProduct: detailProduct,
 	};
 	componentDidMount() {
 		this.setProducts();
@@ -56,6 +58,18 @@ class ProductProvider extends Component {
 		);
 	};
 
+	openModal = (id) => {
+		const product = this.getItem(id);
+		this.setState(() => {
+			return { modalProduct: product, modalOpen: true };
+		});
+	};
+
+	closeModal = () => {
+		this.setState(() => {
+			return { modalOpen: false };
+		});
+	};
 	render() {
 		return (
 			<ProductContext.Provider
@@ -63,6 +77,8 @@ class ProductProvider extends Component {
 					...this.state,
 					handleDetail: this.handleDetail,
 					addToCart: this.addToCart,
+					openModal: this.openModal,
+					closeModal: this.closeModal,
 				}}
 			>
 				{this.props.children}
